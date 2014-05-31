e52b94017128b55dd1b3062631ca45fdf0ef47f9
kaakaa <stooner.hoe@gmail.com>
2014-05-31 16:29:53 +0900

    output dot file text

--- src/main/scala/org/kaakaa/classpath/entry/ContainerEntry.scala
+++ src/main/scala/org/kaakaa/classpath/entry/ContainerEntry.scala
@@ -5,5 +5,9 @@ import scala.xml.{Node, Elem}
 /**
  * Created by kaakaa_hoe on 2014/05/29.
  */
-class ContainerEntry(entry: Node) extends ClasspathEntry {
+case class ContainerEntry(entry: Node) extends ClasspathEntry {
+  override def getPath(): String = {
+    entry \ "@path" text
+  }
 }
+
